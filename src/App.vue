237424e6518b5cedac15faac84081cc57d47d8db
daisuke <kida@softbrain.co.jp>
2022-04-28 16:49:11 +0900

    途中経過報告用

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
+    <!-- <v-app-bar
       app
       color="primary"
       dark
@@ -35,22 +35,23 @@
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
-    </v-app-bar>
+    </v-app-bar> -->
 
     <v-main>
-      <HelloWorld/>
+      <mSM></mSM>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
+import mSM from './components/mSM.vue';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    mSM,
   },
 
   data: () => ({
