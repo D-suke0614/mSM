ae4bd7216e2aa87fa4e8cea6f611125ffc058bb1
wakarr <wakasugi@softbrain.co.jp>
2022-05-06 14:04:03 +0900

    add route

--- src/router.js
+++ src/router.js
@@ -4,6 +4,10 @@ import Router from 'vue-router'
 import Calender from './components/Calender/Calender.vue'
 import ClientDetail from './components/Client/ClientDetail.vue'
 import ClientIndex from './components/Client/ClientList.vue'
+import ProjectList from './components/Project/ProjectList.vue'
+import ActivityList from './components/Activity/ActivityList.vue'
+import EmployeeList from './components/Employee/User/EmployeeList.vue'
+import AdminList from './components/Employee/Admin/AdminList.vue'
 
 Vue.use(Router)
 
@@ -24,6 +28,27 @@ export default new Router({
       path: '/clients/detail',
       name: 'client_detail',
       component: ClientDetail,
-    }
+    },
+    {
+      path: '/projects',
+      name: 'project',
+      component: ProjectList,
+    },
+    {
+      path: '/activities',
+      name: 'activity',
+      component: ActivityList,
+    },
+    {
+      path: '/employees',
+      name: 'employee',
+      component: EmployeeList,
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: AdminList,
+    },
+
   ]
 })
